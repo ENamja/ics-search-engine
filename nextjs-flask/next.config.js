974bf3b3db544316e68b397b34339e355697f12b
elisha <namelisha@gmail.com>
2024-02-24 18:15:43 -0800

    Uninstall bundle analyzer and cross env

--- nextjs-flask/next.config.js
+++ nextjs-flask/next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = require('@next/bundle-analyzer')({
+// const nextConfig = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+//   rewrites: async () => {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination:
+//           process.env.NODE_ENV === "development"
+//             ? "http://127.0.0.1:8080/api/search/:path*"
+//             : "/api/search/",
+//       },
+//     ];
+//   },
+//   reactStrictMode: true,
+// });
+
+const nextConfig = {
   enabled: process.env.ANALYZE === 'true',
   rewrites: async () => {
     return [
@@ -13,6 +29,6 @@ const nextConfig = require('@next/bundle-analyzer')({
     ];
   },
   reactStrictMode: true,
-});
+};
 
 module.exports = nextConfig;
